6890ac49213ff455ffdb2c775a4d4a473de18454
Lucas-TA <dev.ascencao@gmail.com>
2024-05-24 18:36:33 -0300

    working with email validation

--- contact.php
+++ contact.php
@@ -1,3 +1,44 @@
+<?php 
+	//initialize
+		$name = $email = $subject = $message = '';
+		$errors = ''; //$errors is separeted to reinforce that it is doing a different job
+	//Process Submitted form
+	if (array_key_exists("send", $_POST)) {
+		//Read Data
+			$name = trim($_POST["name"]);
+			$email = trim($_POST["email"]);
+			$subject = trim($_POST["subject"]);
+			$message = trim($_POST["message"]);
+
+		//Check Data
+			$errors = array();
+
+			if (!$name) $errors[] = 'Missing Name';
+				elseif (preg_match('/\r|\n/', $name)) 
+				$errors[] = 'Invalid name';
+
+			if (!$email) $errors[] = 'Missing Email Address';
+				elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+				$errors[] = 'Invalid Email Address';
+
+			if (!$subject) $errors[] = 'Missing Subject';
+				elseif (preg_match('/\r|\n/', $subject)) 
+				$errors[] = 'Invalid subject';
+
+			if (!$message) $errors[] = 'Missing Message';
+
+	if (!$errors) { //If no errors, Send Email
+
+	}
+	else { //Else, Report Errors
+		$errors = sprintf('<p class="errors">%s</p>',
+					implode('<br>', $errors));
+	}
+
+		
+
+	};
+?>
 <?php
 	//	Page Title and Heading
 		$pagetitle = 'Contact';
@@ -12,18 +53,18 @@
 		<article id="contact">
 <!-- if not sent: -->
 			<form id="contact-form" method="get" action="/resources/testform.css" novalidate>
-				[errors]
+				<?= $errors ?>
 				<p><label for="name">Name:</label>
-					<input name="name" id="name" type="text" value="[name]">
+					<input name="name" id="name" type="text" value="<?= $name ?>">
 				</p>
 				<p><label for="email">Email:</label>
-					<input name="email" id="email" type="text" value="[email]">
+					<input name="email" id="email" type="text" value="<?= $email ?>">
 				</p>
 				<p><label for="subject">Subject:</label>
-					<input name="subject" id="subject" type="text" value="[subject]">
+					<input name="subject" id="subject" type="text" value="<?= $subject ?>">
 				</p>
 				<p><label for="message">Message:</label>
-					<textarea name="message" id="message">[message]</textarea>
+					<textarea name="message" id="message"><?= $message ?></textarea>
 				<p><button type="submit" name="send">Send Message</button></p>
 			</form>
 <!-- else:
